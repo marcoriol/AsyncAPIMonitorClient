3025d7735efe930afc3fd406b8bfdf4ceaf61b04
Marc Oriol <marc.oriol@gmail.com>
2022-08-19 10:42:51 +0200

    1st commit

---
+++ AsyncAPIMonitorClient/src/monitoring/MetricsDescriptor.java
@@ -0,0 +1,14 @@
+package monitoring;
+
+public class MetricsDescriptor {
+
+	public MetricsDescriptor(String[] metricNames) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getMeasureIntruments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
