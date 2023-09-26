bd1c903cab9c21546d9e10022a9283b11f0641f6
marc <marc.oriol@gmail.com>
2023-09-26 18:16:48 +0200

    modified the class Monitor to print the notifications of messages sent and received.

--- AsyncAPIMonitorClient/src/monitoring/Monitor.java
+++ AsyncAPIMonitorClient/src/monitoring/Monitor.java
@@ -2,4 +2,25 @@ package monitoring;
 
 public class Monitor {
 
+	public Monitor(Object measureIntruments) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void notifyMessageReceived(String clientIDFor, String name, String messageId, long currentTimeMillis) {
+		System.out.println("Message received by " + clientIDFor);
+		System.out.println("- in channel  " + name);
+		System.out.println("- message ID  " + messageId);
+		System.out.println("- at time " + currentTimeMillis );		
+		System.out.flush();
+	}
+
+	public void notifyMessageSent(String clientIDFor, String name, String messageId, long currentTimeMillis) {
+		System.out.println("Message sent by " + clientIDFor);
+		System.out.println("- in channel  " + name);
+		System.out.println("- message ID  " + messageId);
+		System.out.println("- at time " + currentTimeMillis );		
+		System.out.flush();
+
+	}
+
 }
